95c732b4673ed012dde2c8fb5eb806f9acc21001
Muteba Ndibu Abraham <mutebaabraham60@gmail.com>
2025-11-03 13:11:25 +0100

    exemple de mot de passe d'un pragramme simple...

---
+++ MotDepasse.py
@@ -0,0 +1,12 @@
+user = "abraham"
+password = "abraham123"
+
+print ("___________________LA PAGE DE CONNEXION____________________") 
+
+user = input("Entrez votre nom : ")
+password = input("Entrez votre mot de passe : ")
+
+if user == "abraham" and password == "abraham123":
+    print("mot de passe valide!!!!")
+else:
+    print("mot de passe invalide!!!!")
